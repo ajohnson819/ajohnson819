9d299c949e98af3c851975a86bfe2462605ad151
Adam <azjohnson819@gmail.com>
2021-06-17 17:59:24 -0400

    these are the requirements for the Code Louisville class

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-This READ.me is about the Volunteer project I am doing for Code Louisville. 
+Project includes a README file that explains the following:
 
-Some of the Features it should have:
-Html: demonstration of structure and order. All data should be easy to read, actionable intel, and pithy. 
-CSS: demonstration of aesthetics. The webpage should be easy to navigage and attractive. 
-JS: Displays time and date.
-Py: Automates a Spreadsheet for the viewer.  
+A one paragraph or longer description of what your project is
+
+Which 3+ features you have included from the below lists to meet the requirements
+
+If needed, any special instructions required for the reviewer to run your project
+
+The readme should be called readme.md or readme.txt and located in the top level of your project folder so that GitHub automatically displays it when viewing your project on GitHub
+
+Choose at least 1 item on the CSS Features List below and implement it in your project
+Choose at least 1 item off the list of JavaScript Features List below and implement it in your project
+Choose at least 1 additional item from any of the lists below and implement it in your project
+
+We recommend you pick a 4th item (or more!) to add, just in case something goes wrong with one of your other items - 3 is only the minimum requirement
---
+++ volunteer.js
@@ -0,0 +1,17 @@
+/*
+Requirements for JavaScript for the Code Louisville Front-End class
+JavaScript Features List
+Create a form (such as a ‘Contact Us’ form), validate at least one field (ex: email is in the correct format) and then use that information on your page somehow. For example, display it back to the user on button click, or change a setting on the page based on a section. Having a form that does nothing on clicking Submit or that just refreshes the page is not enough - you must capture the value(s) of the form and use it somehow.
+Create a JavaScript function that performs some form of mathematical operation (calculates something) and displays the result on your page or otherwise uses that value to do something on the site.
+Create and populate a JavaScript array with one or more values and display the contents of some or all of the array on your page
+Create a Javascript loop that dynamically displays HTML on your page - for example displaying a number of list items <li> based on how many times you loop
+Create at least 3 variables (var, const, or let), set their values in JavaScript, and display the values somewhere on your page
+Create one or more JavaScript objects (such as with multiple values contained in { }’s), modify at least one value of that object, and display it on your site
+Show/hide one or more content areas or elements on your site through clicking a button or some other user interaction - must be done with some JavaScript code. 
+Allow the user to rearrange or move items by clicking and dragging an element on your site
+Other features can be added to this list with mentor or staff permission, but we want to see you stretch your skills, so you’ll want to pick something challenging.
+
+Ask your mentors or staff if there’s something you want to do but doesn’t fit into these requirements. As long as it’s a good demonstration of your programming skills, it almost certainly will count!  Basically, we just want to see you do something interesting and challenging, so don’t let these requirements stifle your creativity!
+
+*/
+
